31f093ceb2a3fc57ecf7fe47eb827bdf10545e52
Alan R <alanrobinson1337@gmail.com>
2024-02-24 05:26:00 +0000

    quick comments tidy

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,7 @@ use dns_lookup::{lookup_host};
 use std::env;
 
 fn main() {
-    //TODO: Extract IPv4 & IPv6 into unique methods that have returns
 
-    // Get domain names from command-line arguments
-    // let args = env::args().skip(1).collect::<Vec<String>>();
-
-    // for domain in args {
-    //     // Perform DNS lookup for IPv4 addresses
-    //     let ipv4_addrs = match lookup_host(&domain) {
-    //         Ok(addrs) => addrs.into_iter()
-    //             .filter(|addr| addr.is_ipv4())
-    //             .collect::<Vec<IpAddr>>(),
-    //         Err(err) => {
-    //             println!("Error: Could not resolve IPv4 addresses for {}: {}", domain, err);
-    //             continue;
-    //         }
-    //     };
-    //
-    //     // Print IPv4 addresses
-    //     for addr in ipv4_addrs {
-    //         println!("{} IPv4 {}", domain, addr);
-    //     }
-
-        // Perform DNS lookup for IPv6 addresses
-        // let ipv6_addrs = match lookup_host(&domain) {
-        //     Ok(addrs) => addrs.into_iter()
-        //         .filter(|addr| addr.is_ipv6())
-        //         .collect::<Vec<IpAddr>>(),
-        //     Err(err) => {
-        //         println!("Error: Could not resolve IPv6 addresses for {}: {}", domain, err);
-        //         continue;
-        //     }
-        // };
-        //
-        // // Print IPv6 addresses
-        // for addr in ipv6_addrs {
-        //     println!("{} IPv6 {}", domain, addr);
-        // }
     let args = env::args().skip(1).collect::<Vec<String>>();
     let ipv4_addrs: Vec<IpDomain> = ipv4(&args);
     let ipv6_addrs: Vec<IpDomain> = ipv6(&args);
@@ -52,9 +16,6 @@ fn main() {
     for addr in ipv6_addrs {
         println!(" IPv6 {:?}",  addr);
     }
-
-    //}
-    //cant use ipv_addrs here
 }
 #[derive(Debug)]
 struct IpDomain {
@@ -65,14 +26,12 @@ struct IpDomain {
     ip_address_port:SocketAddr
 }
 
-
 fn add_port(ip_addr: IpAddr) -> SocketAddr{
     let ip_addr_str = ip_addr.to_string();
     let ip_addr = ip_addr_str.parse::<IpAddr>().unwrap();
     let socket_addr = SocketAddr::new(ip_addr, 80);
     return socket_addr
 }
-
 fn ipv4(ipv4domains: &Vec<String>) -> Vec<IpDomain> {
     let mut ipv4_addrs_return = vec![];
     for domain in ipv4domains {
